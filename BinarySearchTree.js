ea85a4471f0939046ccc9c61385454f8474f792f
andfwas <andfwas@gmail.com>
2018-01-12 13:34:47 -0700

    initial commit

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -2,45 +2,56 @@ const Node = require('./Node')
 const Queue = require('./Queue')
 
 class BinarySearchTree {
-	constructor() {
-		this.root = null
-	}
+  constructor() {
+    this.root = null
+  }
 
-	insert(data) {
-		if(!this.root) {
-			this.root = new Node(data)
-		} else {
-			let currentNode = this.root
+  insert(data) {
+    if (!this.root) {
+      this.root = new Node(data)
+    } else {
+      let currentNode = this.root
 
-			while(true) {
-				if(data <= currentNode.data) {
-					if(currentNode.left) {
-						currentNode = currentNode.left
-					} else {
-						currentNode.left = new Node(data)
-						break
-					}
-				} else {
-					if(currentNode.right) {
-						currentNode = currentNode.right
-					} else {
-						currentNode.right = new Node(data)
-						break
-					}
-				}
-			}
-		}
-	}
+      while (true) {
+        if (data <= currentNode.data) {
+          if (currentNode.left) {
+            currentNode = currentNode.left
+          } else {
+            currentNode.left = new Node(data)
+            break
+          }
+        } else {
+          if (currentNode.right) {
+            currentNode = currentNode.right
+          } else {
+            currentNode.right = new Node(data)
+            break
+          }
+        }
+      }
+    }
+  }
 
-	dfs(cb, node=this.root) {
-    // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
-		// Expected order: '25, 15, 17, 197, 97, 9997'
-	}
+  dfs(cb, node = this.root) {
+		if (!node) return
+		cb(node.data)
+		this.dfs(cb, node.left)
+		this.dfs(cb, node.right)
+  }
 
-	bfs(cb, node=this.root) {
-    // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
-    // Expected order: 25, 15, 197, 17, 97, 9997
-	}
+  bfs(cb, node = this.root) {
+    var q = new Queue
+    if (node)
+      q.enqueue(node)
+    while (q.queue.length > 0) {
+      const current = q.dequeue()
+      cb(current.data);
+      if (current.left)
+        q.enqueue(current.left)
+      if (current.right)
+        q.enqueue(current.right)
+    }
+  }
 }
 
 module.exports = BinarySearchTree
